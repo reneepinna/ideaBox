1086abb8f7a4dba066f7ffa3f318615881ca0bb5
Renee Pinna <renee.pinna@gmail.com>
2023-06-03 12:12:54 -0600

    Fix: Fix bug that caused an error when rendering favorite ideas

--- main.js
+++ main.js
@@ -8,32 +8,37 @@ var showStarredButton = document.querySelector('.show-starred-button');
 
 // global variables
 
-var userIdeas = [];
+var userIdeas = []; //save ALL user ideas
 var favoriteUserIdeas = [];
 
 // event listeners
 
 saveButton.addEventListener('click', function(event) {
+// when the save button is clicked, IF title and input are filled
   if (titleInput.value && bodyInput.value) {
-    saveUserInput(event);
-    displayIdeaCards(userIdeas);
-    toggleSaveButtonState();
+    saveUserInput(event); // creates a userIdeaObject and pushes it into userIdeas array (title, body, date.now id, isFavorite = false, class = '')
+    displayIdeaCards(userIdeas); // display ALL of the userIdeas array
+    clearUserInput() // clears the user input
+    toggleSaveButtonState(); // The button is a lighter color and not a pointer while inputs are empty
   }
-
 });
-bodyInput.addEventListener('keyup', toggleSaveButtonState);
-titleInput.addEventListener('keyup', toggleSaveButtonState);
+
+bodyInput.addEventListener('keyup', toggleSaveButtonState); //check if BOTH inputs are filled when user types in body input
+
+titleInput.addEventListener('keyup', toggleSaveButtonState); //check if BOTH inputs are filled when user types in title input
+
+
 ideaGrid.addEventListener('click', function(event) {
   console.log(event.target.className)
-  if (event.target.className === 'delete-button') {
-    deleteUserIdeaCard(event) 
-  }else if (event.target.className.includes('favorite-button')) {
-    favoriteUserIdeaCard(event)
+  if (event.target.className === 'delete-button') { //if the user clicks a delete button
+    deleteUserIdeaCard(event); // remove the card from the user idea array and display ALL user ideas
+  }else if (event.target.className.includes('favorite-button')) { //if the user clicks a favorite button
+    favoriteUserIdeaCard(event);
   }
 });
 
 showStarredButton.addEventListener('click', function() {
-  displayIdeaCards(favoriteUserIdeaCard);
+  displayIdeaCards(favoriteUserIdeas);
 });
 
 // functions
@@ -41,16 +46,17 @@ showStarredButton.addEventListener('click', function() {
 function favoriteUserIdeaCard(event) {
  for (var i = 0; i < userIdeas.length; i++) {
   if(parseInt(event.target.closest('.user-idea-card').id) === userIdeas[i].id) {
-    if  (userIdeas[i].isFavorite) {
-      userIdeas[i].isFavorite = false;
-      favoriteUserIdeas.splice( i, 1);
-      event.target.closest('.favorite-button').classList.remove('active');
+    if  (userIdeas[i].isFavorite) { //if the card is already favorites and user clicks the star button
+      userIdeas[i].isFavorite = false; //it should be unfavorited
+      favoriteUserIdeas.splice( i, 1); //it should be removed from the favorite user ideas array
+      event.target.closest('.favorite-button').classList.remove('active'); //it should show a white star
+      userIdeas[i].class = ''; //the class for interpolation should be changed so it will always render correctly
 
     } else {
-      userIdeas[i].isFavorite = true;
-      favoriteUserIdeas.push(userIdeas[i]);
-      event.target.closest('.favorite-button').classList.add('active');
-      userIdeas[i].class = " active"
+      userIdeas[i].isFavorite = true; //if it is not favorited it should be favorited
+      favoriteUserIdeas.push(userIdeas[i]); //it should be added to the favorite user ideas array
+      event.target.closest('.favorite-button').classList.add('active'); // it should show with an orange star
+      userIdeas[i].class = " active"; // it should have the active class for interpolation for rendering
 
     }
         
@@ -59,24 +65,18 @@ function favoriteUserIdeaCard(event) {
   }
 }
 
-function deleteUserIdeaCard(event) {
+function deleteUserIdeaCard(event) { //possible rename???
   for (var i = 0; i < userIdeas.length; i++) {
     if (parseInt(event.target.closest('.user-idea-card').id) === userIdeas[i].id) {
       userIdeas.splice(i, 1);
-    } 
-  }
-
-  for (var i = 0; i < favoriteUserIdeaCard.length; i++){
-    if(parseInt(event.target.closest('.user-idea-card').id) === favoriteUserIdeas[i].id) {
-      favoriteUserIdeas.splice(i, 1);
     } 
   }
   displayIdeaCards(userIdeas);
 }
 
 function saveUserInput() {
-    var newUserIdea = createUserObject(titleInput.value, bodyInput.value);
-    userIdeas.push(newUserIdea);
+    var newUserIdea = createUserObject(titleInput.value, bodyInput.value); //creates a new object
+    userIdeas.push(newUserIdea); 
     console.log(userIdeas); 
 }
 
@@ -107,8 +107,6 @@ function displayIdeaCards(ideas){
             <h2>${ideas[i].title}</h2>
             <h4>${ideas[i].body}</h4></article>`
     }
-
-     clearUserInput()
 }
 
 function toggleSaveButtonState() {
